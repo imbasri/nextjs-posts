5594d2b54cab11eb4282014c8f35071db2356b40
imbasri <imbasri.dev@gmail.com>
2025-02-10 16:26:26 +0700

    fix error deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    webpack5: true,
-    images: {
-        domains: ['localhost'],
-    },
-    async headers() {
-        return [
-        {
-            source: '/(.*)',
-            headers: securityHeaders,
-        },
-        ];
-    },
-    async redirects() {
-        return [
-        {
-            source: '/posts',
-            destination: '/',
-            permanent: true,
-        },
-        ];
-    },
-    async rewrites() {
-        return [
-        {
-            source: '/posts',
-            destination: '/',
-        },
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
